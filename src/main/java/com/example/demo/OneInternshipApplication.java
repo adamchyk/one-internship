d426b19bf037b845689e059abf0151bcc8d235bf
andrei <andreiadamchyk@gmail.com>
2021-08-09 13:36:57 -0600

    first commit

---
+++ src/main/java/com/example/demo/OneInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneInternshipApplication.class, args);
+	}
+
+}
