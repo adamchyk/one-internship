0c2136b39a245dc14d79ede05ab9547e457c91e2
Artem Makhonka <artemmakhonko@gmail.com>
2021-08-11 12:18:46 -0500

    Updated SQL queries for create tables with SQL constraints

--- db/init.sql
+++ db/init.sql
@@ -1,31 +1,31 @@
 create table users (
-    id              int         not null primary key,
-    username        varchar(20) not null,
-    password        varchar(20) not null,
-    is_admin        boolean not null,
-    account_enabled boolean not null
+   id              int         not null primary key,
+   username        varchar(20) not null,
+   password        varchar(20) not null,
+   is_admin        boolean not null,
+   account_enabled boolean not null,
+       foreign key(id) references users(id)
+           on delete cascade
 );
 
 create table categories (
     category_id int not null primary key,
-     owner_id int not null,
-     name varchar(20)
-);
-
-create table notes (
-    note_id int not null primary key,
-    category_id int not null,
     owner_id int not null,
-    note text
-    Primary key (id)
-    CONSTRAINT fk_categories
-    Foreign key (owner_id) REFERENCES notes(owner_id)
-    Foreign key (owner_id) REFERENCES categories (owner_id)
-    ON DELETE CASCADE;
-
-    Primary key (category_id)
-    CONSTRAINT fk_notes
-    Foreign key (category_id) REFERENCES notes (category_id)
-    ON DELETE CASCADE
+    name varchar(20)
 );
+
+create table notes
+(
+    note_id     int not null primary key,
+    category_id int not null,
+    owner_id    int not null,
+    note        text,
+    constraint fk_notes_categories,
+    foreign key (category_id) references categories (category_id)
+        on delete cascade,
+    constraint fk_notes_users,
+    foreign key(owner_id) references users (id)
+        on delete cascade
+);
+
 
