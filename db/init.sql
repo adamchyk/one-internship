8d716a1095459086e2e2d7beef32021fe24f927e
Artem Makhonka <artemmakhonko@gmail.com>
2021-08-11 14:51:23 -0500

    Updated SQL queries for create tables with SQL constraints and updated type instead int to serial

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,5 @@
 create table users (
-   id              int         not null primary key,
+   id              serial      not null primary key,
    username        varchar(20) not null,
    password        varchar(20) not null,
    is_admin        boolean not null,
@@ -9,14 +9,14 @@ create table users (
 );
 
 create table categories (
-    category_id int not null primary key,
+    category_id serial not null primary key,
     owner_id int not null,
     name varchar(20)
 );
 
 create table notes
 (
-    note_id     int not null primary key,
+    note_id     serial not null primary key,
     category_id int not null,
     owner_id    int not null,
     note        text,
