4f0cd52c03f07b30e76c26c140881b1c15a501a1
Andrei Adamchyk <andreiadamchyk@gmail.com>
2021-08-16 07:48:27 -0500

    updated UserService

--- src/main/java/com/one/internship/controller/NotesController.java
+++ src/main/java/com/one/internship/controller/NotesController.java
@@ -12,6 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,12 +33,37 @@ public class NotesController {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private EntityManager em;
+
     @GetMapping("/notes")
     public List<NoteInfo> getNotes(@RequestParam(required = false) Integer categoryId,
                                    @RequestParam(required = false) String noteContains,
                                    Principal principal) {
         List<NoteInfo> noteInfos = new ArrayList<>();
-        List<Note> noteList = noteRepository.findAll();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Note> query = cb.createQuery(Note.class);
+        Root<Note> noteRoot = query.from(Note.class);
+        List<Predicate> predicates = new ArrayList<Predicate>();
+
+        // filter 1
+        User currentUser = userRepository.findByUsername(principal.getName()).get();
+        predicates.add(cb.equal(noteRoot.get("owner").get("id"), currentUser.getId()));
+
+        // filter 2
+        if (categoryId != null) {
+            predicates.add(cb.equal(noteRoot.get("category").get("categoryId"), categoryId));
+        }
+
+        // filter 3
+        if (noteContains != null && !noteContains.isBlank()) {
+            predicates.add(cb.and(cb.like(noteRoot.get("note"), '%' + noteContains + '%')));
+        }
+        query.select(noteRoot).where(predicates.toArray(new Predicate[0]));
+
+        // add filtration by noteContains
+        List<Note> noteList = em.createQuery(query).getResultList();
+
         for (int i = 0; i < noteList.size(); i++) {
             NoteInfo noteInfo = new NoteInfo();
             Note n = noteList.get(i);
