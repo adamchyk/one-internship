70b874464f81f5577a43aeac53869c32c7c0b799
Andrei Adamchyk <andreiadamchyk@gmail.com>
2021-08-16 08:29:17 -0500

    updated SecurityConfig

--- src/main/java/com/one/internship/config/SecurityConfig.java
+++ src/main/java/com/one/internship/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.one.internship.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,6 +18,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserService userService;
+
+
+    @Bean("authenticationManager")
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
