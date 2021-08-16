640e13d08658bd7fd0ae82b534e7da179b38ab07
Andrei Adamchyk <andreiadamchyk@gmail.com>
2021-08-16 09:04:22 -0500

    updated SignupController

--- src/main/java/com/one/internship/controller/SignupController.java
+++ src/main/java/com/one/internship/controller/SignupController.java
@@ -13,11 +13,14 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @RestController
@@ -61,8 +64,11 @@ public class SignupController {
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<?> logoutUser() {
-
+    public ResponseEntity<?> logoutUser(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
         return ResponseEntity.ok(new MessageResponse("ok"));
     }
 }
